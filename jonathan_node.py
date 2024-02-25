cbb8108412481f77848357b100a18b33edbd8a7b
Jonathan Boxall <jonathan.boxall@students.plymouth.ac.uk>
2024-02-25 01:57:59 +0000

    cleaned up code, got firebase connection working, sends a log of who has accessed when

--- jonathan_node.py
+++ jonathan_node.py
@@ -1,55 +1,92 @@
 #!/usr/bin/env python
-import rospy
-import cv2
-from std_msgs.msg import String
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
+# coding: utf-8
+
+# Importing modules
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import db,credentials,storage,initialize_app
+import face_recognition as fr
+from datetime import datetime
 
-cred = credentials.Certificate("dissertation-fd159-firebase-adminsdk-yfsdf-cf7f19ee4a.json")
-firebase_admin.initialize_app(cred , {
-    'databaseURL' : 'https://dissertation-fd159-default-rtdb.europe-west1.firebasedatabase.app/',
-    'storageBucket' : 'gs://dissertation-fd159.appspot.com'
-})
+# Authentication for database
+if not firebase_admin._apps:
+    # Authentication for database
+    cred = credentials.Certificate("Certificate.json")
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://dissertation-fd159-default-rtdb.europe-west1.firebasedatabase.app/',
+        "storageBucket": "dissertation-fd159.appspot.com"
+    })
 
-class image_converter:
-    def __init__(self):
-        self.bridge = CvBridge()
-        self.image_received = False
+# Reference to firebase Storage Bucket
+bucket = storage.bucket('dissertation-fd159.appspot.com')
 
-    def image_callback(self, data):
-        if not self.image_received:
-            try:
-                cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-                cv2.imwrite("/home/qtrobot/catkin_ws/src/jonathan/src/image.jpg", cv_image)
-                self.image_received = True
-                print("Image saved.")
-            except CvBridgeError as e:
-                print("CvBridgeError:", e)
+# Reference to firebase database
+ref = db.reference('/recognised_faces_log')
 
-def main():
-    rospy.init_node('jonathan_node')
-    rospy.loginfo("jonathan_node started!")
+# Downloads accepted face image from database when called
+def download_image(remote_path, local_path):
+    blob = bucket.blob(remote_path)
+    blob.download_to_filename(local_path)
+    print(f"Image downloaded to {local_path}")
 
-    speechSay_pub = rospy.Publisher('/qt_robot/speech/say', String, queue_size=10)
-    rospy.sleep(5.0)  # Increased delay for speech setup
-    speechSay_pub.publish("Hello! I am going to capture an image now.")
+remote_image_path = "Faces/jonathan.jpg"  # Path to the image in Firebase Storage
+local_image_path = "downloaded_image.jpg"  # Local path where the image will be downloaded
+download_image(remote_image_path, local_image_path)
 
-    ic = image_converter()
-    rospy.Subscriber("/camera/color/image_raw", Image, ic.image_callback)
+remote_image_path = "Faces/Phil.jpg"  # Path to the image in Firebase Storage
+local_image_path = "phil_image.jpg"  # Local path where the image will be downloaded
+download_image(remote_image_path, local_image_path)
 
-    while not ic.image_received and not rospy.is_shutdown():
-        rospy.sleep(0.1)
 
-    rospy.sleep(2.0)  # Delay to ensure speech message is processed
-    rospy.loginfo("Image capture complete. Node will shutdown.")
-    rospy.signal_shutdown("Image Captured")
+# In[144]:
 
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("Shutting down")
+
+# Load known images
+trainedImage = fr.load_image_file("downloaded_image.jpg")
+trainedImage2 = fr.load_image_file("phil_image.jpg")
+
+# Encode known images
+try:
+    trainedEnc = fr.face_encodings(trainedImage)[0]
+    trainedEnc2 = fr.face_encodings(trainedImage2)[0]
+except IndexError:
+    print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+    quit()
+
+# List of recognised faces and their names
+known_faces = {
+    "Jonathan": trainedEnc,
+    "Phil": trainedEnc2
+    }
+
+# Load and encode unknown images - Try/Except to handle any errors where the image hasn't been found or no faces are found
+try:
+    unknownImage = fr.load_image_file("unknown.jpg")
+except (FileNotFoundError, IOError):
+    print("Wrong file or file path")
+try:
+    unknownEnc = fr.face_encodings(unknownImage)[0]
+except IndexError:
+    print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+    quit()
+
+# Defining a list to store recognised names
+recognised_names = []
+
+# Check for recognised faces
+for name, encoding in known_faces.items():
+    if fr.compare_faces([encoding], unknownEnc)[0]:
+        recognised_names.append(name)
+
+# Update the log on the database for who was recognised and when - Useful for access logs when fully implemented 
+if recognised_names:
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    for name in recognised_names:
+        ref.push({
+            "name": name,
+            "date_time": current_time
+        })
+    print("Hello", name + ",", "your face has been recognised successfully")
+else:
+    print("No recognised faces.")
 
 
